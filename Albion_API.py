3c02d96f25b5ec05105f58fbd3734742f187730e
Anderson Nascimento <Anderson.pnf_@outlook.com>
2024-11-19 22:04:11 -0300

    Update Albion_API.py

--- Albion_API.py
+++ Albion_API.py
@@ -4,13 +4,14 @@ Url = "https://old.west.albion-online-data.com/api/v2/stats/prices/t4_bag"
 
 params = {
     "locations": "Bridgewatch, Lymhurst",
-    "qualities": "1,2,3"
+    "qualities": "1,2,3,4"
 }
 
 response = requests.get(Url, params=params)
 
 if response.status_code == 200:
-    print("Dados retornados com sucesso")
-    print(response.json())
+    data = response.json()
+    for item in data:
+        print(f"Item: {item['item_id']} {item['sell_price_min']} {item['quality']}")
 else:
     print(f"{response.status_code}, {response.text}")
