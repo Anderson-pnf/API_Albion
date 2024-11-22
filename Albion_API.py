a35e4680ec83457a07791e5c85f8d8c2de7b22b7
Anderson Nascimento <Anderson.pnf_@outlook.com>
2024-11-21 21:18:04 -0300

    Update Albion_API.py

--- Albion_API.py
+++ Albion_API.py
@@ -1,17 +1,27 @@
+import pandas as pd
 import requests
 
-Url = "https://old.west.albion-online-data.com/api/v2/stats/prices/t4_bag"
+url = "https://old.west.albion-online-data.com/api/v2/stats/prices/t4_bag,t4_bag@1,t4_bag@2,t4_bag@3,t4_bag@4,t5_bag,t6_bag,t7_bag,t8_bag"
 
 params = {
-    "locations": "Bridgewatch, Lymhurst",
-    "qualities": "1,2,3,4"
+    "locations": "Bridgewatch, Lymhurst, Caerleon, Thetford, Martlock, Fort Sterling, Black Marketing",
+    "qualities": "1,2,3,4",
+    "enchantment": "1,2,3,4"
 }
 
-response = requests.get(Url, params=params)
+response = requests.get(url, params=params)
 
 if response.status_code == 200:
     data = response.json()
-    for item in data:
-        print(f"Item: {item['item_id']} {item['sell_price_min']} {item['quality']}")
+    print(data)
+
+    if isinstance(data, list):
+
+        df = pd.DataFrame(data)
+        df.to_excel("Dados_Albion.xlsx", index=False)
+
+    else:
+        print("A resposta não é uma lista. Verifique a estrutura dos dados.")
+
 else:
     print(f"{response.status_code}, {response.text}")
